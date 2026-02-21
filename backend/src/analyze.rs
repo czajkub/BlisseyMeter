fa1dd719f78e8332e4da560ef4e77fa6bee2012f
czajkub <czajkub@gmail.com>
2026-02-21 17:08:45 +0100

    some 1st config

---
+++ backend/src/analyze.rs
@@ -0,0 +1,12 @@
+use crate::handlers::handle_join;
+
+pub async fn analyze(lines: Vec<String>) {
+    for line in &lines {
+        let mut split = line.split('|');
+        let line_type = split.next().unwrap_or_default();
+        match line_type {
+            "j" => handle_join(split),
+            _ => "".to_string(),
+        };
+    }
+}
---
+++ backend/src/main.rs
@@ -0,0 +1,26 @@
+mod fetch;
+mod analyze;
+mod handlers;
+
+use fetch::fetch_replay;
+use analyze::analyze;
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[post("/analyze", data = "<replay_uri>")]
+async fn analyze_replay(replay_uri: String) -> String {
+    let lines = fetch_replay(&replay_uri).await.unwrap_or_default();
+    analyze(lines).await;
+    format!("Analysis complete")
+
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, analyze_replay])
+}
